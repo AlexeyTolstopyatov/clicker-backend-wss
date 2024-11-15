d0db30fd28b8f64894efc6b67907ab9c4dc38fd8
Alexey Tolstopyatov <att33pod@gmail.com>
2024-11-15 16:04:37 +0700

    debugging client commit

---
+++ Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using WebSocket = WebSocketSharp.WebSocket;
+
+namespace wsst.Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        WebSocket wss = new("ws://localhost:8080/ws");
+        wss.Connect();
+        bool isDead = false;
+        
+        while (!isDead)
+        {
+            Console.Write("Send to server:");
+            string request = Console.ReadLine()!;
+
+            switch (request)
+            {
+                case ":x":
+                    isDead = true;
+                    break;
+                case ":init":
+                    wss.Send(File.ReadAllBytes(AppDomain.CurrentDomain.BaseDirectory + "init.json"));
+                    break;
+                case ":score":
+                    wss.Send(File.ReadAllBytes(AppDomain.CurrentDomain.BaseDirectory + "score.json"));
+                    break;
+                case ":text":
+                    string data = Console.ReadLine()!;
+                    wss.Send(data);
+                    break;
+                default:
+                    Console.WriteLine("Unknown command");
+                    break;
+            }
+        }
+        
+        Console.ReadLine();
+        wss.Close();
+    }
+}
